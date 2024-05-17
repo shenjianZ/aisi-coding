50d46dee5e7469d62427c8d632c6a305dcb9fbaf
shenjianZ <shenjianZLT@gmail.com>
2024-05-17 19:03:03 +0800

    feat Baidu Analytics

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,7 @@ export default defineUserConfig({
   title: "爱思文档",
   description: "美丽的事物，总是很棘手",
   head: [
+    // Google Analytics
     ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-2MZYS7X7RD' }],
     [
       'script',
@@ -17,6 +18,22 @@ export default defineUserConfig({
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-2MZYS7X7RD');
+        console.log('Google Analytics script loaded');
+      `,
+    ],
+    // Baidu Analytics
+    [
+      'script',
+      {},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?8176022f1d4be778ca77323ca39f9abb";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+          console.log('Baidu Analytics script loaded');
+        })();
       `,
     ],
   ],
