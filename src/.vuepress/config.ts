5efe73d9172b03f43f46d6b8f3b6df00b1a47975
shenjianZ <shenjianZLT@gmail.com>
2024-05-17 18:04:15 +0800

    feat google Analytics

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,11 +7,21 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "爱思文档",
   description: "美丽的事物，总是很棘手",
+  head: [
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-2MZYS7X7RD' }],
+    [
+      'script',
+      {},
+      `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-2MZYS7X7RD');
+      `,
+    ],
+  ],
   theme,
   plugins: [
-    googleAnalyticsPlugin({
-      id: "G-2MZYS7X7RD",
-    }),
   ],
   markdown:{
     headers:{
